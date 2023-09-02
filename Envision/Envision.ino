c68c8156f04ce9aa5d35de838b913a45b467059f
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2023-09-02 22:32:45 +0700

    New version 0.1.5

--- Envision/Envision.ino
+++ Envision/Envision.ino
@@ -143,7 +143,7 @@ void setup() {
 
   lcd16x2.print("Envision - HfA");
   lcd16x2.setCursor(1 - 1, 2 - 1);
-  lcd16x2.print("V.0.1.4 BETA");
+  lcd16x2.print("V.0.1.5 BETA");
   Serial.print("Envision IDE\n");
   tone(9, 1760, 500);
   delay(500);
@@ -171,7 +171,7 @@ void loop() {
       lcd16x2.clear();
       lcd16x2.print("Envision - HfA");
       lcd16x2.setCursor(1 - 1, 2 - 1);
-      lcd16x2.print("V.0.1.4 BETA");
+      lcd16x2.print("V.0.1.5 BETA");
       // / LOG / // Serial.print("Log : reset\n");
     }
   }
