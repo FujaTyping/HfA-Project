08d9e11bbc27c24a2fe863ab0b40a22191777a87
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-08-25 18:49:57 +0700

    [NOT TEST] Add DFPlayer in source file

--- HfA-Source/HfA/HfA.ino
+++ HfA-Source/HfA/HfA.ino
@@ -1,13 +1,18 @@
 // Require Libray :
 // Adafruit Unified Sensor
 // DHT sensor library
+// EspSoftwareSerial
+// DFRobotDFPlayerMini
 
 #include "BluetoothSerial.h"
+#include "SoftwareSerial.h"
 #include "DHT.h"
+#include "DFRobotDFPlayerMini.h"
 
 String DeviceName = "SmartHomevision";
 String CMD = "";
 String SerialByte = "";
+int DFVolume = 50;
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
@@ -21,8 +26,12 @@ BluetoothSerial SerialBT;
 
 #define DHTPIN 16
 #define DHTTYPE DHT11
+#define DFTXPIN 27
+#define DFRXPIN 25
 
+SoftwareSerial softwareSerial(DFRXPIN, DFTXPIN);
 DHT dht(DHTPIN, DHTTYPE);
+DFRobotDFPlayerMini DFPlayer;
 
 void setup() {
   pinMode(26, OUTPUT);
@@ -30,7 +39,14 @@ void setup() {
   pinMode(19, OUTPUT);
   pinMode(23, OUTPUT);
   Serial.begin(115200);
+  softwareSerial.begin(9600);
   SerialBT.begin(DeviceName);
+  if (DFPlayer.begin(softwareSerial)) {
+    Serial.println("[LOG] : DFPlayer connected");
+    DFPlayer.volume(DFVolume);
+  } else {
+    Serial.println("[LOG] : Can't connect to DFPlayer");
+  }
 }
 
 void loop() {
@@ -44,34 +60,68 @@ void loop() {
   }
   if (SerialBT.available() > 0) {
     CMD = SerialBT.readStringUntil('\n');
-    Serial.println(CMD);
-    if(CMD == "LC") {
-      digitalWrite(26, false);
-    }
-    if(CMD == "LO") {
-      digitalWrite(26, true);
-    }
-    if(CMD == "LRO") {
-      digitalWrite(18, true);
-    }
-    if(CMD == "LRF") {
-      digitalWrite(18, false);
-    }
-    if(CMD == "LGO") {
-      digitalWrite(19, true);
-    }
-    if(CMD == "LGF") {
-      digitalWrite(19, false);
-    }
-    if(CMD == "LBO") {
-      digitalWrite(23, true);
-    }
-    if(CMD == "LBF") {
-      digitalWrite(23, false);
-    }
+    Serial.println("[CMD] : "+CMD);
+    Commands(CMD);
   }
   SerialBT.println(SerialByte);
   delay(1000);
+}
+
+void Commands(String CMD) {
+  if(CMD == "LC") {
+    digitalWrite(26, false);
+  }
+  if(CMD == "LO") {
+    digitalWrite(26, true);
+  }
+  if(CMD == "LRO") {
+    digitalWrite(18, true);
+  }
+  if(CMD == "LRF") {
+    digitalWrite(18, false);
+  }
+  if(CMD == "LGO") {
+    digitalWrite(19, true);
+  }
+  if(CMD == "LGF") {
+    digitalWrite(19, false);
+  }
+  if(CMD == "LBO") {
+    digitalWrite(23, true);
+  }
+  if(CMD == "LBF") {
+    digitalWrite(23, false);
+  }
+  if(CMD == "DFPLofi") {
+    DFPlayer.play(5);
+  }
+  if(CMD == "DFPRock") {
+    DFPlayer.play(3);
+  }
+  if(CMD == "DFPClassic") {
+    DFPlayer.play(4);
+  }
+  if(CMD == "DFPOrchestra") {
+    DFPlayer.play(2);
+  }
+  if(CMD == "DFPPop") {
+    DFPlayer.play(1);
+  }
+  if(CMD == "DFVU") {
+    if(DFVolume <= 100) {
+      DFVolume += 10;
+      DFPlayer.volume(DFVolume);
+    }
+  }
+  if(CMD == "DFVD") {
+    if(DFVolume >= 0) {
+      DFVolume -= 10;
+      DFPlayer.volume(DFVolume);
+    }
+  }
+  if(CMD == "DFPU") {
+    DFPlayer.pause();
+  }
 }
 
 int randint() {
